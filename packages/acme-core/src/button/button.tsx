d1ba4f2bc8014c9f67df997ee4f4bb48788889c9
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2023-03-03 06:09:33 +0700

    Change: button component, add mantine, and config

--- packages/acme-core/src/button/button.tsx
+++ packages/acme-core/src/button/button.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import { Button as MantineButton } from "@mantine/core";
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <MantineButton>{props.children}</MantineButton>;
 }
 
 Button.displayName = "Button";
